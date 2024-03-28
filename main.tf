e89f6760cf033261c46bfc46bf2eb788fbd9977f
Kelvinskell <kelvinskelll@gmail.com>
2024-03-28 03:56:19 +0100

    Create VPC

--- main.tf
+++ main.tf
@@ -12,3 +12,29 @@ provider "aws" {
   region  = var.region
   profile = "default"
 }
+
+# Define local values
+locals {
+  environment = "Prod"
+  purpose     = "ECS App" 
+}
+
+# Create A VPC
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "ecs-vpc"
+  cidr = "10.20.0.0/22"
+
+  azs             = var.azs
+  private_subnets = ["10.20.0.0/25", "10.20.0.128/25", "10.20.1.0/25"]
+  public_subnets  = ["10.20.1.128/25", "10.20.2.0/25", "10.20.2.128/25"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Environment = local.environment
+    Purpose = local.purpose
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
 variable "region" {
   default = "us-east-1"
+  type = string
+}
+
+variable "azs" {
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  type = list
 }
