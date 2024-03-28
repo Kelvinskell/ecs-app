24e82b00594655d0de0d113235a883aae38d5a69
Kelvinskell <kelvinskelll@gmail.com>
2024-03-28 04:33:57 +0100

    rename module

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "vpc" {
 
 # Create security groups
 module "security_groups" {
-  source = "./modules/security-groups"
+  source = "./modules/security_groups"
 
   vpc_id = module.vpc.vpc_id
 }
